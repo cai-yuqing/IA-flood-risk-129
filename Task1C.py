f004494e35ee6b0b9899524921d6083c374ee51c
Xie Yuxuan <yx353@cam.ac.uk>
2022-02-09 18:10:49 +0000

    edited function name

--- Task1C.py
+++ Task1C.py
@@ -1,4 +1,4 @@
-from floodsystem.geo import station_within_radius
+from floodsystem.geo import stations_within_radius
 from floodsystem.stationdata import build_station_list
 from floodsystem.haversine import haversine
 from floodsystem.station import MonitoringStation
@@ -10,7 +10,7 @@ def run():
     stations = build_station_list()
 
     #station within 10 km from required coordinates
-    print(station_within_radius(stations, (52.2053, 0.1218), 10))
+    print(stations_within_radius(stations, (52.2053, 0.1218), 10))
 
 if __name__ == "__main__":
     print("*** Task 1C: CUED Part IA Flood Warning System ***")
